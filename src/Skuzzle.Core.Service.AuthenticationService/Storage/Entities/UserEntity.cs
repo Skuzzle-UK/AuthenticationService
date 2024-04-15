63b1d04f6baeadd7f815eb5f7a713c1ea5fa2b3d
Nick.Bailey <Nick.Bailey@swarco.com>
2024-04-15 13:06:47 +0100

    Added todo

--- src/Skuzzle.Core.Service.AuthenticationService/Storage/Entities/UserEntity.cs
+++ src/Skuzzle.Core.Service.AuthenticationService/Storage/Entities/UserEntity.cs
@@ -4,6 +4,65 @@ using System.ComponentModel.DataAnnotations;
 
 namespace Skuzzle.Core.Service.AuthenticationService.Storage.Entities;
 
+// TODO Pop UserEntity into an encrypted repo here's a suggestion:
+/*password encryption bash script
+    echo -n "UnencryptedPassword" > input.txt
+
+    key='ac06487adaedae424878915aac1234567897aacab422a7272a0a04ff4aa13145'
+
+    ivec='1aa1cd4f56787ba787a78ddc9e108ae2'
+ 
+    encoded=$(openssl enc -aes-256-cbc -nosalt -e \
+        -in input.txt \
+        -K $key -iv $ivec | openssl base64)
+
+    echo $encoded*/
+
+/*public class Encrypter
+{
+    private readonly byte[] _key;
+    private readonly byte[] _iv;
+
+    public Encrypter(string keyString, string ivString)
+    {
+        _key = Convert.FromHexString(keyString);
+        _iv = Convert.FromHexString(ivString);
+    }
+
+    public string EncryptString(string text)
+    {
+        var plainText = Encoding.UTF8.GetBytes(text);
+
+        using var aesAlg = Aes.Create();
+        using var encryptor = aesAlg.CreateEncryptor(_key, _iv);
+        using var msEncrypt = new MemoryStream();
+        using (var csEncrypt = new CryptoStream(msEncrypt, encryptor, CryptoStreamMode.Write))
+        {
+            csEncrypt.Write(plainText, 0, plainText.Length);
+        }
+
+        var cipherText = msEncrypt.ToArray();
+
+        return Convert.ToBase64String(cipherText);
+    }
+
+    public string DecryptString(string cipherText)
+    {
+        var fullCipher = Convert.FromBase64String(cipherText);
+
+        using var aesAlg = Aes.Create();
+        using var decryptor = aesAlg.CreateDecryptor(_key, _iv);
+        using var msDecrypt = new MemoryStream();
+        using (var csDecrypt = new CryptoStream(msDecrypt, decryptor, CryptoStreamMode.Write))
+        {
+            csDecrypt.Write(fullCipher, 0, fullCipher.Length);
+        }
+
+        var plaintext = msDecrypt.ToArray();
+        return Encoding.UTF8.GetString(plaintext);
+    }
+}*/
+
 public class UserEntity : IEntity
 {
     [Key]
