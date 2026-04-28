741f93acef41fa48aff6afbb104d054eed529fac
Nick Bailey <Nick.Bailey@activate-group.com>
2026-04-28 10:15:14 +0100

    Removed warning in console and improved docs in controller

--- AuthenticationService/Controllers/TestController.cs
+++ AuthenticationService/Controllers/TestController.cs
@@ -2,10 +2,18 @@
 using Microsoft.AspNetCore.Mvc;
 
 namespace AuthenticationService.Controllers;
+
+/// <summary>
+/// Controller for testing out authentication/authorization scenarios
+/// </summary>
 [Route("api/[controller]")]
 [ApiController]
 public class TestController : ControllerBase
 {
+    /// <summary>
+    /// A test endpoint locked down to only admin
+    /// </summary>
+    /// <returns>Ok if successful auth else returns 401</returns>
     [HttpGet]
     [Authorize(Policy = "OnlyAdminUsers")]
     public IActionResult TestAdminOnlyAsync()
@@ -13,6 +21,10 @@ public class TestController : ControllerBase
         return Ok("Test succeeded");
     }
 
+    /// <summary>
+    /// A test endpoint for all authenticated users
+    /// </summary>
+    /// <returns>Ok if user token is valid and authenticated else returns 401</returns>
     [HttpGet("all")]
     [Authorize]
     public IActionResult TestAllAuthenticatedUsersAsync()
